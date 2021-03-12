143ef32f3ce6c7baac71981f4bed8a9d751943da
radan devist <radandevist@gmail.com>
2021-03-12 08:40:02 +0300

    config update

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,7 @@ const config = {
     mongoUri:
         process.env.MONGODB_URI ||
         process.env.MONGO_HOST ||
-        `mongodb://${process.env.IP || "localhost"}:${process.env.MONGO_PORT || "27017"}/${process.env.DB_NAME || "api-express"}`,
+        `mongodb://${process.env.IP || "localhost"}:${process.env.MONGO_PORT || "27017"}/${process.env.DB_NAME || "api-express-ts"}`,
   },
   jwt: {
     secret: process.env.JWT_SECRET || "api-secret-key",
@@ -16,8 +16,8 @@ const config = {
     cookieMaxAge: 24 * 60 * 60 * 1000, // 24h in milliseconds
   },
   siteAdmin: {
-    name: "superadmin",
-    email: "superadmin@gmail.com",
+    name: process.env.ADMIN_NAME || "superadmin",
+    email: process.env.ADMIN_EMAIL || "superadmin@gmail.com",
     password: process.env.ADMIN_PASS || "1234567",
   },
 };
