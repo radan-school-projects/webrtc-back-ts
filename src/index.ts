73992111d39b6bcd50c7da333f3d70666f37b53f
radan devist <radandevist@gmail.com>
2021-06-30 16:22:39 +0300

    simple one to one webrtc

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,49 @@
 
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:3000",
+      "http://192.168.8.100:3000",
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+const rooms: {[key: string]: string[]} = {};
+
+io.on("connection", (socket) => {
+  socket.on("join room", (roomID) => {
+    if (rooms[roomID]) {
+      rooms[roomID].push(socket.id);
+    } else {
+      rooms[roomID] = [socket.id];
+    }
+    const otherUser = rooms[roomID].find((id) => id !== socket.id);
+    if (otherUser) {
+      socket.emit("other user", otherUser);
+      socket.to(otherUser).emit("user joined", socket.id);
+    }
+  });
+
+  socket.on("offer", (payload) => {
+    io.to(payload.target).emit("offer", payload);
+  });
+
+  socket.on("answer", (payload) => {
+    io.to(payload.target).emit("answer", payload);
+  });
+
+  socket.on("ice-candidate", (incoming) => {
+    io.to(incoming.target).emit("ice-candidate", incoming.candidate);
+  });
+});
+
+const port = 3300;
+server.listen(port, () => console.log("server is running on port %s", port));
