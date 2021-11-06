0f8ef603e8c5a8b775993624049098c92a09e1c2
Andrianarisoa Daniel <radandevist@gmail.com>
2021-11-06 13:47:28 +0300

    📂 show a friendly page when someone get to this server's url

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import express from "express";
+import path from "path";
+import express, { Request, Response } from "express";
 import http from "http";
+// import https from "https";
 import { Server } from "socket.io";
+// import cors from "cors";
+
 import { connectionEventHandler } from "./app/websocket";
 import {
   usernameValidation,
@@ -12,6 +16,13 @@ import { PORT, serverOptions } from "./config";
 
 // initialization
 const app = express();
+// app.use(cors());
+app.use(express.static(path.join(process.cwd(), "static")));
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(process.cwd(), "welcome.html"));
+});
+
+// const httpServer = https.createServer(app);
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, serverOptions);
 
@@ -25,5 +36,6 @@ io.on("connection", connectionEventHandler);
 // server listen
 httpServer.listen(
     PORT,
+    // "192.168.1.119",
     () => console.log(`Runnning on port: ${PORT}`),
 );
